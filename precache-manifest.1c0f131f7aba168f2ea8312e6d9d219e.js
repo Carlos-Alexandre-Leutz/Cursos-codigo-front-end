2424ed45c615f119acb1d1f1fa1cbd1b1bac1ee0
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-11-04 15:32:38 -0300

    pos biud login curso

---
+++ precache-manifest.1c0f131f7aba168f2ea8312e6d9d219e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b552e22d455e4f0e0f0",
+    "url": "/Cursos-codigo-front-end/css/app.ddc3178a.css"
+  },
+  {
+    "revision": "4797c7110d2099f81ae7",
+    "url": "/Cursos-codigo-front-end/css/chunk-29ef60ec.f3ef123a.css"
+  },
+  {
+    "revision": "12e26ea5ee58a3fe9217",
+    "url": "/Cursos-codigo-front-end/css/chunk-6bdc7cf2.8971f386.css"
+  },
+  {
+    "revision": "4967c54fe4d606a6510e",
+    "url": "/Cursos-codigo-front-end/css/chunk-796645fa.bcc475c4.css"
+  },
+  {
+    "revision": "1cb68d6cee717bb5345ca968bf57900a",
+    "url": "/Cursos-codigo-front-end/index.html"
+  },
+  {
+    "revision": "5b552e22d455e4f0e0f0",
+    "url": "/Cursos-codigo-front-end/js/app.8bd7065d.js"
+  },
+  {
+    "revision": "06012868f65e1b9a7d11",
+    "url": "/Cursos-codigo-front-end/js/chunk-281c80c9.742773b4.js"
+  },
+  {
+    "revision": "4797c7110d2099f81ae7",
+    "url": "/Cursos-codigo-front-end/js/chunk-29ef60ec.c5905c26.js"
+  },
+  {
+    "revision": "cbcf6687ee8b17cdab25",
+    "url": "/Cursos-codigo-front-end/js/chunk-2d22d746.6bfd0836.js"
+  },
+  {
+    "revision": "26906fbc44ee5b5d2bc2",
+    "url": "/Cursos-codigo-front-end/js/chunk-46ea1636.7845c958.js"
+  },
+  {
+    "revision": "12e26ea5ee58a3fe9217",
+    "url": "/Cursos-codigo-front-end/js/chunk-6bdc7cf2.ba6f98df.js"
+  },
+  {
+    "revision": "4967c54fe4d606a6510e",
+    "url": "/Cursos-codigo-front-end/js/chunk-796645fa.36588de9.js"
+  },
+  {
+    "revision": "39bf589353a3c07dbc10",
+    "url": "/Cursos-codigo-front-end/js/chunk-f3e8821c.85474454.js"
+  },
+  {
+    "revision": "1623eb24c24028bc1c1f",
+    "url": "/Cursos-codigo-front-end/js/chunk-vendors.aad6a7e9.js"
+  },
+  {
+    "revision": "2a8aa7b08861f47ed5c3ece6d1dd398c",
+    "url": "/Cursos-codigo-front-end/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Cursos-codigo-front-end/robots.txt"
+  }
+]);
